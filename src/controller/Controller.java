89f13b9a0f5d583d5285c2dbe081f44f776cbfac
Tulliola <fulviocoppola28@gmail.com>
2025-08-06 11:35:51 +0200

    Ho definito la classe DBConnection e l'eccezione NomeSchemaException

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,11 @@
 package controller;
+
 import gui.FrameDiLogin;
+import java.sql.*;
 
 public class Controller {
-
+	private Connection connessione = null;
+		
 	public static void main(String args[]) {
 		
 	}
