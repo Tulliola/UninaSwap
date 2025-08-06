943bc99dc72aa88f28e973d08e113754db768448
justantxnio <contean0013@gmail.com>
2025-08-06 11:58:40 +0200

    Prima versione del frame di login

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,8 +2,11 @@ package controller;
 import gui.FrameDiLogin;
 
 public class Controller {
-
+	public Controller() {
+		FrameDiLogin loginFrame = new FrameDiLogin(this);
+	}
+	
 	public static void main(String args[]) {
-		
+		Controller mainController = new Controller();
 	}
 }
