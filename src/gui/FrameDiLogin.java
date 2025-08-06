ee0ff0764985566515c007e9c12f7e0af8e36a06
justantxnio <contean0013@gmail.com>
2025-08-06 20:23:39 +0200

    Prima versione del frame di Login terminata

--- src/gui/FrameDiLogin.java
+++ src/gui/FrameDiLogin.java
@@ -11,28 +11,35 @@ public class FrameDiLogin extends JFrame {
 	//TODO aggiungere variabili di istanza
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel fieldPane;
+	private JPanel buttonsPane;
+	private JTextField emailField;
+	private JPasswordField passwordField;
+	private JButton accediButton = new JButton("Accedi");
+	private JButton registratiButton = new JButton("Registrati");
 	private Controller mainController;
 	
 	public FrameDiLogin(Controller controller) {
 		mainController = controller;
 		
+		//Impostazione del frame di base
 		impostaSettingsPerFrame();
 		
-		//aggiunta del logo dell'applicativo UninaSwap
+		//Aggiunta del logo dell'applicativo UninaSwap
 		aggiungiLogo(new ImageIcon("images/logo_uninaswap.png"));
 		
-		//aggiunta dello slogan dell'applicativo UninaSwap
+		//Aggiunta dello slogan dell'applicativo UninaSwap
 		aggiungiMotto();
 		
-		contentPane.add(Box.createRigidArea(new Dimension(30, 30)));
+		contentPane.add(Box.createRigidArea(new Dimension(25, 25)));
 
-		//aggiunta delle Lable e dei TextField necessari per l'accesso
+		//Aggiunta delle Lable e dei TextField necessari per l'accesso
 		aggiungiFieldAccesso();
+		
+		contentPane.add(Box.createRigidArea(new Dimension(20, 20)));
 	
-		//aggiunta dei bottoni per l'accesso
-		JButton accedi = new JButton("Accedi");
-		JButton registrati = new JButton("Registrati");
-		aggiungiBottoniAccesso(accedi, registrati);
+		//Aggiunta dei bottoni per l'accesso
+		aggiungiBottoniLogin();
 		
 	}
 	
@@ -47,41 +54,48 @@ public class FrameDiLogin extends JFrame {
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Accesso");
-		setContentPane(contentPane);
+		this.setContentPane(contentPane);
 		
 	}
 	
 	private void aggiungiFieldAccesso() {
-		JPanel pannelloFieldDatiAccesso = new JPanel();
-		pannelloFieldDatiAccesso.setLayout(new BoxLayout(pannelloFieldDatiAccesso, BoxLayout.Y_AXIS));
+		fieldPane = new JPanel();
+		fieldPane.setBackground(Color.WHITE);
+		fieldPane.setLayout(new BoxLayout(fieldPane, BoxLayout.Y_AXIS));
 		
-		pannelloFieldDatiAccesso.setBackground(Color.WHITE);
+		JLabel campiObbligatori = new JLabel("I campi col simbolo * sono obbligatori");
+		campiObbligatori.setAlignmentX(LEFT_ALIGNMENT);
+		fieldPane.add(campiObbligatori);	
 		
-		JLabel email = new JLabel("Email istituzionale o username");
-		JTextField inserisciEmail = new JTextField(10);
-		inserisciEmail.setMaximumSize(new Dimension(300, 30));
-		email.setAlignmentX(LEFT_ALIGNMENT);
-		inserisciEmail.setAlignmentX(LEFT_ALIGNMENT);
+		fieldPane.add(Box.createRigidArea(new Dimension(25, 25)));
 		
-		pannelloFieldDatiAccesso.add(email);
-		pannelloFieldDatiAccesso.add(inserisciEmail);
+		JLabel email = new JLabel("Email istituzionale o username *");
+		emailField = new JTextField();
+		aggiungiFieldAccesso(email, emailField);
 		
+		fieldPane.add(email);
+		fieldPane.add(emailField);
 		
-		JLabel password = new JLabel("Password");
-		JPasswordField inserisciPassword = new JPasswordField();
-		inserisciPassword.setMaximumSize(new Dimension(300, 30));
-		password.setAlignmentX(LEFT_ALIGNMENT);
-		inserisciPassword.setAlignmentX(LEFT_ALIGNMENT);
+		fieldPane.add(Box.createRigidArea(new Dimension(15, 15)));
 		
-		pannelloFieldDatiAccesso.add(password);
-		pannelloFieldDatiAccesso.add(inserisciPassword);
+		JLabel password = new JLabel("Password *");
+		passwordField = new JPasswordField();
+		aggiungiFieldAccesso(password, passwordField);
 		
-		
-		pannelloFieldDatiAccesso.setAlignmentX(CENTER_ALIGNMENT);
-		pannelloFieldDatiAccesso.setAlignmentY(CENTER_ALIGNMENT);
+		fieldPane.add(password);
+		fieldPane.add(passwordField);
 		
 		
-		contentPane.add(pannelloFieldDatiAccesso);
+		fieldPane.setAlignmentX(CENTER_ALIGNMENT);
+		
+		
+		contentPane.add(fieldPane);
+	}
+	
+	private void aggiungiFieldAccesso(JLabel field, JTextField text) {
+		text.setMaximumSize(new Dimension(300, 30));
+		field.setAlignmentX(LEFT_ALIGNMENT);
+		text.setAlignmentX(LEFT_ALIGNMENT);
 	}
 	
 	private void aggiungiLogo(ImageIcon img) {
@@ -90,62 +104,51 @@ public class FrameDiLogin extends JFrame {
 		ImageIcon logo = new ImageIcon(resizedImg);
 		etichettaImmagine.setIcon(logo);
 		etichettaImmagine.setAlignmentX(CENTER_ALIGNMENT);
-		etichettaImmagine.setAlignmentY(CENTER_ALIGNMENT);
 		
 		contentPane.add(etichettaImmagine);
 		
 	}
 	
-	private void aggiungiBottoniAccesso(JButton accedi, JButton registrati) {
-		JPanel pannelloBottoni = new JPanel();
-		pannelloBottoni.setLayout(new BoxLayout(pannelloBottoni, BoxLayout.Y_AXIS));
-		pannelloBottoni.setBackground(Color.WHITE);
+	private void aggiungiBottoniLogin() {
+		buttonsPane = new JPanel();
+		buttonsPane.setLayout(new BoxLayout(buttonsPane, BoxLayout.Y_AXIS));
+		buttonsPane.setAlignmentX(CENTER_ALIGNMENT);
+		buttonsPane.setBackground(Color.WHITE);
 		
-		accedi.setAlignmentX(CENTER_ALIGNMENT);
-		accedi.setAlignmentY(BOTTOM_ALIGNMENT);
-		accedi.setBackground(new Color(65, 106, 144));
-		accedi.setForeground(Color.WHITE);
-		accedi.setFocusable(false);
+		aggiungiBottoniLogin(accediButton);
 		
-		registrati.setAlignmentX(CENTER_ALIGNMENT);
-		registrati.setAlignmentY(BOTTOM_ALIGNMENT);
-		registrati.setBackground(pannelloBottoni.getBackground());
-		registrati.setFocusable(false);
-		
-		JLabel oppureRegistrati = new JLabel ("Oppure se non sei ancora registrato, fallo!");
+		JLabel oppureRegistrati = new JLabel ("Oppure, se non sei ancora registrato, fallo!");
 		oppureRegistrati.setAlignmentX(CENTER_ALIGNMENT);
-		JTextPane pane = new JTextPane();
-
 		
-		pannelloBottoni.add(accedi);
-		pannelloBottoni.add(oppureRegistrati);
-		pannelloBottoni.add(registrati);
+		aggiungiBottoniLogin(registratiButton);		
 		
-		contentPane.add(pannelloBottoni);
+		buttonsPane.add(accediButton);
+		buttonsPane.add(Box.createRigidArea(new Dimension(20, 20)));
+		buttonsPane.add(oppureRegistrati);
+		buttonsPane.add(Box.createRigidArea(new Dimension(10, 10)));
+		buttonsPane.add(registratiButton);
+		
+		contentPane.add(buttonsPane);
+	}
+	
+	private void aggiungiBottoniLogin(JButton button) {
+		button.setAlignmentX(CENTER_ALIGNMENT);
+		button.setBackground(new Color(65, 106, 144));
+		button.setForeground(Color.WHITE);
+		button.setFocusable(false);
 	}
 
 	private void aggiungiMotto() {
-		JPanel pannelloMotto = new JPanel();
-		pannelloMotto.setBackground(Color.WHITE);
-		pannelloMotto.setLayout(new BoxLayout(pannelloMotto, BoxLayout.Y_AXIS));
-		
 		JLabel nome = new JLabel("UninaSwap");
-		nome.setAlignmentX(CENTER_ALIGNMENT);
-		nome.setAlignmentY(TOP_ALIGNMENT);
 		nome.setFont(new Font("UninaSwap", Font.BOLD, 25));
 		nome.setForeground(new Color(65, 106, 144));
-		
+		nome.setAlignmentX(CENTER_ALIGNMENT);
 		JLabel motto = new JLabel("Il mercatino digitale federiciano");
-		motto.setAlignmentX(CENTER_ALIGNMENT);
-		motto.setAlignmentY(BOTTOM_ALIGNMENT);
 		motto.setFont(new Font("Motto", Font.ITALIC, 16));
 		motto.setForeground(new Color(65, 106, 144));
+		motto.setAlignmentX(CENTER_ALIGNMENT);
 		
-		pannelloMotto.add(nome);
-		pannelloMotto.add(motto);
-		
-		pannelloMotto.setAlignmentX(CENTER_ALIGNMENT);
-		pannelloMotto.setAlignmentY(CENTER_ALIGNMENT);
-		contentPane.add(pannelloMotto);
+		contentPane.add(nome);
+		contentPane.add(motto);
 	}
 }
