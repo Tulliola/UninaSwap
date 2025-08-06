1927c218be29572847e12d5b7d12b483c8a30b5b
justantxnio <contean0013@gmail.com>
2025-08-06 12:19:57 +0200

    modificato il file FrameDiLogin

--- src/gui/FrameDiLogin.java
+++ src/gui/FrameDiLogin.java
@@ -1,14 +1,10 @@
 package gui;
 
-import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.*;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+
 import controller.Controller;
 
 public class FrameDiLogin extends JFrame {
@@ -18,11 +14,36 @@ public class FrameDiLogin extends JFrame {
 	private Controller mainController;
 	public FrameDiLogin(Controller controller) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
+//		contentPane.setLayout(new BoxLayout(contentPane, defaultCloseOperation));
+		this.setResizable(false);
+		this.setTitle("Accesso");
 		setContentPane(contentPane);
+		
+		//Logo UninaSwap
+		JLabel logoUninaSwap = new JLabel();
+		ImageIcon logoDaScalare = new ImageIcon("images/logo_uninaswap.png");
+		Image resizedLogo = logoDaScalare.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+		ImageIcon logoUS = new ImageIcon(resizedLogo);
+		logoUninaSwap.setIcon(logoUS);
+		logoUninaSwap.setVerticalAlignment(JLabel.CENTER);
+		logoUninaSwap.setHorizontalAlignment(JLabel.CENTER);
+		contentPane.add(logoUninaSwap);
+		
+		
+		//Label per indicare di inserire la mail
+		JLabel email = new JLabel("Email o username");
+		contentPane.add(email);
 
+		//TextField in cui inserire la mail
+		JTextField inserisciEmail = new JTextField(20);
+		inserisciEmail.setBounds(50, 50, 50, 50);
+		contentPane.add(inserisciEmail);
+		
+		this.setVisible(true);
 	}
 
 }
