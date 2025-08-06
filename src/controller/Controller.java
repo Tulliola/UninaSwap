a2eab030981c9086138a80d7049b98c27a267880
justantxnio <contean0013@gmail.com>
2025-08-06 12:29:42 +0200

    Riprova di aggiunta frame di login

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,43 +5,14 @@ import java.sql.*;
 //Import dal package GUI
 import gui.FrameDiLogin;
 
-<<<<<<< HEAD
+
 public class Controller {
 	public Controller() {
 		FrameDiLogin loginFrame = new FrameDiLogin(this);
-	}
+	}	
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		Controller mainController = new Controller();
-=======
-//Import dal package Database
-import database.DBConnection;
-import eccezioni.NomeSchemaException;
-
-public class Controller {
-	private Connection connessione = null;
-	
-	//Prova di connessione
-	public Controller() {
-		DBConnection dbConnection = DBConnection.getConnessioneAlDB();
-		
-		try {
-			connessione = dbConnection.connettitiTramiteSchema("uninaswap");
-
-			if(connessione == null) {
-				System.out.println("Non è stato possibile definire una connessione con il DB.");
-				System.exit(0);
-			}
-			else
-				System.out.println("Connessione al DB riuscita.");
-		}
-		catch(NomeSchemaException eccezione) {
-			System.out.println(eccezione.getMessage());
-		}
-	}
-	
-	public static void main(String args[]) {
-		Controller controller = new Controller();
->>>>>>> main
 	}
 }
+
