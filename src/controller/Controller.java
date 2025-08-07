06de4e0ae6233859f8a9c76b3726ddbf7703d116
justantxnio <contean0013@gmail.com>
2025-08-07 10:59:58 +0200

    Versione app con frame di login e di registrazione statici

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,18 +7,18 @@ import gui.*;
 
 
 public class Controller {
-//	private FrameDiLogin frameDiLogin;
+	private FrameDiLogin frameDiLogin;
 	private FrameDiRegistrazione frameDiRegistrazione;
 	
 	public Controller() {
+		frameDiLogin = new FrameDiLogin(this);
+		frameDiLogin.setVisible(true);
 		frameDiRegistrazione = new FrameDiRegistrazione(this);
 		frameDiRegistrazione.setVisible(true);
 		
-		//FrameDiLogin loginFrame = new FrameDiLogin(this);
 	}	
 	
 	public static void main(String[] args) {
 		Controller mainController = new Controller();
 	}
 }
-
